1d484164a3621f37c6c06fd780d9ed8dfb8e8caf
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-12 18:37:02 -0700

    added more sidebar row components

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './Sidebar.css';
-import SidebarRow from './SidebarRow';
-import HomeIcon from '@material-ui/icons/Home';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
-import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import React from "react";
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <SidebarRow Icon={HomeIcon} title="Home" />
-            <SidebarRow Icon={WhatshotIcon} title="Trending" />
-            <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
-            
-            
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <SidebarRow selected Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
+
+      <hr></hr>
+
+      <Sidebar Icon={VideoLibraryIcon} title="Library" />
+      <Sidebar Icon={HistoryIcon} title="History" />
+      <Sidebar Icon={OndemandVideoIcon} title="Your Videos" />
+      <Sidebar Icon={WatchLaterIcon} title="Watch Later" />
+      <Sidebar Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+      <Sidebar Icon={ExpandMoreOutlinedIcon} title="Show More" />
+
+      <hr></hr>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
