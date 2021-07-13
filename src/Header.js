970ae79410aeac37593862e6c87dc03d8bac6163
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-13 08:48:11 -0700

    adding in search functunality

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import Avatar from '@material-ui/core/Avatar'
+import Avatar from '@material-ui/core/Avatar';
+import { Link } from 'react-router-dom';
 
 function Header() {
+
+    const [inputSearch, setInputSearch] = useState('');
+
+
+
     return (
         <div className='header'>
 
@@ -17,8 +23,10 @@ function Header() {
             </div>
 
             <div className='header_input'>
-            <input placeholder='Search' type='text'></input>
+            <input onChange={e => setInputSearch(e.target.value)} value={inputSearch} placeholder='Search' type='text'></input>
+            <Link to={`/search/${inputSearch}`}>
             <SearchIcon className='header_inputButton'/>
+            </Link>
             </div>
 
             <div className='header_right'>
